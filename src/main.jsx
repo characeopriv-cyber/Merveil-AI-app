78170d2f0f796c5e50159b83d86d6fe3a0b33a80
IVONIX CEO <characeopriv@gmail.com>
2026-09-02 18:21:13 +0400

    Expose Merveil Interface as separate platform route

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import PlusHub from "./plusHub.jsx";
 import MerveilSoundsPlayer from "./MerveilSoundsPlayer.jsx";
 import MerveilAdminControlCenter from "./MerveilAdminControlCenter.jsx";
+import MerveilInterfacePlatform from "./MerveilInterfacePlatform.jsx";
 import "./index.css";
 
-const isAdminPath = typeof window !== "undefined" && window.location.pathname === "/merveil-admin-x9k2";
+const path = typeof window !== "undefined" ? window.location.pathname.replace(/\/+$/, "") || "/" : "/";
+const isAdminPath = path === "/merveil-admin-x9k2";
+const isInterfacePath = path === "/interface" || path === "/interface/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isAdminPath ? <MerveilAdminControlCenter /> : <><App /><PlusHub /><MerveilSoundsPlayer /></>}
+    {isAdminPath ? <MerveilAdminControlCenter /> : isInterfacePath ? <MerveilInterfacePlatform /> : <><App /><PlusHub /><MerveilSoundsPlayer /></>}
   </React.StrictMode>
 );
