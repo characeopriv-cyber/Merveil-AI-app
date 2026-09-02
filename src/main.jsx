e3c3b74103b15287c4dc6a0ce4d68c1c645b96fb
IVONIX CEO <characeopriv@gmail.com>
2026-09-02 16:17:41 +0400

    Route private admin into native Merveil Control Center

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import PlusHub from "./plusHub.jsx";
+import AdminControlCenter from "./AdminControlCenter.jsx";
 import "./index.css";
+
+const isAdminPath = typeof window !== "undefined" && window.location.pathname === "/merveil-admin-x9k2";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <React.Fragment>
-      <App />
-      <PlusHub />
-    </React.Fragment>
+    {isAdminPath ? <AdminControlCenter /> : <><App /><PlusHub /></>}
   </React.StrictMode>
 );
