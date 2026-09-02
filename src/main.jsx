61cc9e741417c35138c6671197d7d8f0df0a8e7d
IVONIX CEO <characeopriv@gmail.com>
2026-09-02 16:42:38 +0400

    Use reorganized Merveil Admin Control Center interface

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import PlusHub from "./plusHub.jsx";
-import AdminControlCenter from "./AdminControlCenter.jsx";
+import MerveilAdminControlCenter from "./MerveilAdminControlCenter.jsx";
 import "./index.css";
 
 const isAdminPath = typeof window !== "undefined" && window.location.pathname === "/merveil-admin-x9k2";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isAdminPath ? <AdminControlCenter /> : <><App /><PlusHub /></>}
+    {isAdminPath ? <MerveilAdminControlCenter /> : <><App /><PlusHub /></>}
   </React.StrictMode>
 );
