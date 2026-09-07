b957dcf9a97e14542f953df08dfb99700883516a
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 03:28:29 +0400

    fix: provide machine service to credential guard

--- machine-connect/core/src/auth/machine-credentials.module.ts
+++ machine-connect/core/src/auth/machine-credentials.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { MachineModule } from '../machine/machine.module';
 import { MachineCredentialsController } from './machine-credentials.controller';
 import { MachineCredentialsService } from './machine-credentials.service';
 
 @Module({
-  imports: [PersistenceModule],
+  imports: [PersistenceModule, MachineModule],
   controllers: [MachineCredentialsController],
   providers: [MachineCredentialsService],
   exports: [MachineCredentialsService],
