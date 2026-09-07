a210f32d8a3baa9f6af26380123fd7ebcee6e8c5
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 21:46:08 +0400

    feat(machine-connect): wire offensive security module

--- machine-connect/core/src/app.module.ts
+++ machine-connect/core/src/app.module.ts
@@ -4,6 +4,9 @@ import { CommandModule } from './command/command.module';
 import { TelemetryModule } from './telemetry/telemetry.module';
 import { AuditModule } from './audit/audit.module';
 import { EmergencyStopModule } from './safety/emergency-stop.module';
+import { OffensiveSecurityModule } from './security/offensive-security.module';
 
-@Module({ imports: [MachineModule, CommandModule, TelemetryModule, AuditModule, EmergencyStopModule] })
+@Module({
+  imports: [MachineModule, CommandModule, TelemetryModule, AuditModule, EmergencyStopModule, OffensiveSecurityModule],
+})
 export class AppModule {}
