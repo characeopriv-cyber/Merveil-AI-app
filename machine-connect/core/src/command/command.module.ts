0aafec40bb2641c14ee8a10e6c8d2cfe82758315
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 22:58:24 +0400

    feat(machine-connect): enable command persistence

--- machine-connect/core/src/command/command.module.ts
+++ machine-connect/core/src/command/command.module.ts
@@ -3,9 +3,10 @@ import { CommandController } from './command.controller';
 import { CommandService } from './command.service';
 import { MachineModule } from '../machine/machine.module';
 import { PolicyService } from '../safety/policy.service';
+import { PersistenceModule } from '../persistence/persistence.module';
 
 @Module({
-  imports: [MachineModule],
+  imports: [MachineModule, PersistenceModule],
   controllers: [CommandController],
   providers: [CommandService, PolicyService],
   exports: [CommandService],
