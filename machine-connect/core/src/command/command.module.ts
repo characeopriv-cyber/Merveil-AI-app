e8bb32c5469d10cfe6b2190ac0b5ebdf122750d1
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 03:11:16 +0400

    feat: enforce emergency stop in command path

--- machine-connect/core/src/command/command.module.ts
+++ machine-connect/core/src/command/command.module.ts
@@ -4,9 +4,10 @@ import { CommandService } from './command.service';
 import { MachineModule } from '../machine/machine.module';
 import { PolicyService } from '../safety/policy.service';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { EmergencyStopModule } from '../safety/emergency-stop.module';
 
 @Module({
-  imports: [MachineModule, PersistenceModule],
+  imports: [MachineModule, PersistenceModule, EmergencyStopModule],
   controllers: [CommandController],
   providers: [CommandService, PolicyService],
   exports: [CommandService],
