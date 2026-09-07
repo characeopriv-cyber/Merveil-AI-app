0282842b97617b2198221ae5085c77b3d4dc7d8a
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 22:58:34 +0400

    feat(machine-connect): enable telemetry persistence

--- machine-connect/core/src/telemetry/telemetry.module.ts
+++ machine-connect/core/src/telemetry/telemetry.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TelemetryController } from './telemetry.controller';
 import { TelemetryService } from './telemetry.service';
+import { PersistenceModule } from '../persistence/persistence.module';
 
-@Module({ controllers: [TelemetryController], providers: [TelemetryService], exports: [TelemetryService] })
+@Module({ imports: [PersistenceModule], controllers: [TelemetryController], providers: [TelemetryService], exports: [TelemetryService] })
 export class TelemetryModule {}
