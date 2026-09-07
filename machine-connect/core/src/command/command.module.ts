0c9b31accd5f61305aca90886df9be7a89fbfc0d
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 03:14:00 +0400

    feat: register command dispatcher service

--- machine-connect/core/src/command/command.module.ts
+++ machine-connect/core/src/command/command.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CommandController } from './command.controller';
 import { CommandService } from './command.service';
+import { CommandDispatcherService } from './command-dispatcher.service';
 import { MachineModule } from '../machine/machine.module';
 import { PolicyService } from '../safety/policy.service';
 import { PersistenceModule } from '../persistence/persistence.module';
@@ -9,7 +10,7 @@ import { EmergencyStopModule } from '../safety/emergency-stop.module';
 @Module({
   imports: [MachineModule, PersistenceModule, EmergencyStopModule],
   controllers: [CommandController],
-  providers: [CommandService, PolicyService],
-  exports: [CommandService],
+  providers: [CommandService, CommandDispatcherService, PolicyService],
+  exports: [CommandService, CommandDispatcherService],
 })
 export class CommandModule {}
