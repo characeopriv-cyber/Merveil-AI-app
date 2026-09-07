fc1139a78cd5c3fdc0f929f57a7254271e04640e
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 22:57:58 +0400

    feat(machine-connect): connect machine domain to persistence

--- machine-connect/core/src/machine/machine.module.ts
+++ machine-connect/core/src/machine/machine.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MachineController } from './machine.controller';
 import { MachineService } from './machine.service';
+import { PersistenceModule } from '../persistence/persistence.module';
 
 @Module({
+  imports: [PersistenceModule],
   controllers: [MachineController],
   providers: [MachineService],
   exports: [MachineService],
