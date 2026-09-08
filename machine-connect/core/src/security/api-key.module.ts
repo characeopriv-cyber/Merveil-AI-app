c7904d9325ca89c6bbee5c214acd7d704f3c6eb2
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 08:37:43 +0400

    security: wire API key security event dependency

--- machine-connect/core/src/security/api-key.module.ts
+++ machine-connect/core/src/security/api-key.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { SecurityEventModule } from './security-event.module';
 import { ApiKeyService } from './api-key.service';
 import { ApiKeyController } from './api-key.controller';
 
 @Module({
-  imports: [PersistenceModule],
+  imports: [PersistenceModule, SecurityEventModule],
   controllers: [ApiKeyController],
   providers: [ApiKeyService],
   exports: [ApiKeyService],
