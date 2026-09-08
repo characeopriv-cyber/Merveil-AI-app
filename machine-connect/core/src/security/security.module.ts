c896902286b3ee801c09942d3d7a89adfadbe0bb
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 08:38:45 +0400

    security: wire rate-limit event dependency

--- machine-connect/core/src/security/security.module.ts
+++ machine-connect/core/src/security/security.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { SecurityEventModule } from './security-event.module';
+import { MfaPolicyModule } from './mfa-policy.module';
 import { RateLimitService } from './rate-limit.service';
 import { RateLimitMiddleware } from './rate-limit.middleware';
-import { MfaPolicyModule } from './mfa-policy.module';
 
 @Module({
-  imports: [PersistenceModule, MfaPolicyModule],
+  imports: [PersistenceModule, SecurityEventModule, MfaPolicyModule],
   providers: [RateLimitService, RateLimitMiddleware],
   exports: [RateLimitService, RateLimitMiddleware, MfaPolicyModule],
 })
