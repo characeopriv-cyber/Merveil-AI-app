381413a6ade30c9a912517036b92951be27e1f00
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 09:40:38 +0400

    feat: register connector provider catalog

--- machine-connect/core/src/connectors/connector.module.ts
+++ machine-connect/core/src/connectors/connector.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { ConnectorController } from './connector.controller';
 import { ConnectorService } from './connector.service';
+import { ConnectorRegistry } from './connector.registry';
 
 @Module({
   imports: [PersistenceModule],
   controllers: [ConnectorController],
-  providers: [ConnectorService],
-  exports: [ConnectorService],
+  providers: [ConnectorService, ConnectorRegistry],
+  exports: [ConnectorService, ConnectorRegistry],
 })
 export class ConnectorModule {}
