24d28c0aef8593553f15821171eb4574fc0260ca
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 20:24:29 +0400

    feat: register MQTT transport adapter

--- machine-connect/core/src/adapters/adapters.module.ts
+++ machine-connect/core/src/adapters/adapters.module.ts
@@ -1,10 +1,15 @@
 import { Global, Module } from '@nestjs/common';
 import { AdapterRegistry } from './adapter-registry';
 import { ReferenceSimulatorAdapter } from './reference-simulator.adapter';
+import { MqttAdapter } from './mqtt.adapter';
+import { MachineCredentialsModule } from '../auth/machine-credentials.module';
+import { TelemetryModule } from '../telemetry/telemetry.module';
+import { CommandModule } from '../command/command.module';
 
 @Global()
 @Module({
-  providers: [AdapterRegistry, ReferenceSimulatorAdapter],
-  exports: [AdapterRegistry, ReferenceSimulatorAdapter],
+  imports: [MachineCredentialsModule, TelemetryModule, CommandModule],
+  providers: [AdapterRegistry, ReferenceSimulatorAdapter, MqttAdapter],
+  exports: [AdapterRegistry, ReferenceSimulatorAdapter, MqttAdapter],
 })
 export class AdaptersModule {}
