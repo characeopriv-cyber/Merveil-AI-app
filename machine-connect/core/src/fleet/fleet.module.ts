64c7520f25792d9e49cace9274d907f8df414add
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 21:04:40 +0400

    feat(machine-connect): wire fleet intelligence module

--- machine-connect/core/src/fleet/fleet.module.ts
+++ machine-connect/core/src/fleet/fleet.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { MachineModule } from '../machine/machine.module';
 import { CommandModule } from '../command/command.module';
+import { TelemetryModule } from '../telemetry/telemetry.module';
 import { FleetController } from './fleet.controller';
 import { FleetService } from './fleet.service';
+import { FleetIntelligenceController } from './fleet-intelligence.controller';
+import { FleetIntelligenceService } from './fleet-intelligence.service';
 
 @Module({
-  imports: [PersistenceModule, MachineModule, CommandModule],
-  controllers: [FleetController],
-  providers: [FleetService],
-  exports: [FleetService],
+  imports: [PersistenceModule, MachineModule, CommandModule, TelemetryModule],
+  controllers: [FleetController, FleetIntelligenceController],
+  providers: [FleetService, FleetIntelligenceService],
+  exports: [FleetService, FleetIntelligenceService],
 })
 export class FleetModule {}
