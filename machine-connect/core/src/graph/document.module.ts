6d839ae0bb0e403bfd08b569fd7191bc4fe83533
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 08:45:39 +0400

    feat: wire intelligence runtime into document module

--- machine-connect/core/src/graph/document.module.ts
+++ machine-connect/core/src/graph/document.module.ts
@@ -3,6 +3,7 @@ import { PersistenceModule } from '../persistence/persistence.module';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
 import { DocumentProcessorService } from './document-processor.service';
+import { IntelligenceRuntimeClient } from './intelligence-runtime.client';
 
-@Module({ imports: [PersistenceModule], controllers: [DocumentController], providers: [DocumentService, DocumentProcessorService], exports: [DocumentProcessorService] })
+@Module({ imports: [PersistenceModule], controllers: [DocumentController], providers: [DocumentService, DocumentProcessorService, IntelligenceRuntimeClient], exports: [DocumentProcessorService] })
 export class DocumentModule {}
