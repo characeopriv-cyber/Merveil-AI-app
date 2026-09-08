b396b35647cefb1465b2027f42860d8e5d0c0286
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 14:31:57 +0400

    feat(machine-connect): wire workflow execution dependencies

--- machine-connect/core/src/workflow/workflow.module.ts
+++ machine-connect/core/src/workflow/workflow.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WorkflowController } from './workflow.controller';
 import { WorkflowService } from './workflow.service';
+import { PersistenceModule } from '../persistence/persistence.module';
+import { OperationsModule } from '../operations/operations.module';
 
-@Module({
-  controllers: [WorkflowController],
-  providers: [WorkflowService],
-  exports: [WorkflowService],
-})
+@Module({ imports:[PersistenceModule,OperationsModule], controllers:[WorkflowController], providers:[WorkflowService], exports:[WorkflowService] })
 export class WorkflowModule {}
