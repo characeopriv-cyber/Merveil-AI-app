86033c0339490749e6d28f8f0e275dd1e691ea24
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 08:34:32 +0400

    feat: expose enterprise security policy

--- machine-connect/core/src/security/security.module.ts
+++ machine-connect/core/src/security/security.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { RateLimitService } from './rate-limit.service';
 import { RateLimitMiddleware } from './rate-limit.middleware';
+import { MfaPolicyModule } from './mfa-policy.module';
 
 @Module({
-  imports: [PersistenceModule],
+  imports: [PersistenceModule, MfaPolicyModule],
   providers: [RateLimitService, RateLimitMiddleware],
-  exports: [RateLimitService, RateLimitMiddleware],
+  exports: [RateLimitService, RateLimitMiddleware, MfaPolicyModule],
 })
 export class SecurityModule {}
