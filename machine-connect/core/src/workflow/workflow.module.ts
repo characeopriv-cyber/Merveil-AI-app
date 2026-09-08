169d8b0cc9e1db81db8a3c170db2dff93fd20ae8
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 14:38:04 +0400

    fix(machine-connect): support workflow and operations orchestration boundary

--- machine-connect/core/src/workflow/workflow.module.ts
+++ machine-connect/core/src/workflow/workflow.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WorkflowController } from './workflow.controller';
 import { WorkflowService } from './workflow.service';
 import { ExecutionWorkerService } from './execution-worker.service';
@@ -6,7 +6,7 @@ import { PersistenceModule } from '../persistence/persistence.module';
 import { OperationsModule } from '../operations/operations.module';
 
 @Module({
-  imports: [PersistenceModule, OperationsModule],
+  imports: [PersistenceModule, forwardRef(() => OperationsModule)],
   controllers: [WorkflowController],
   providers: [WorkflowService, ExecutionWorkerService],
   exports: [WorkflowService],
