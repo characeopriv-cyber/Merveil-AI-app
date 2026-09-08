18f7d80be6a14489c69aab9004db940d3931ee04
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 15:44:01 +0400

    fix: configure Jest for Machine Connect tests

---
+++ machine-connect/core/jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  clearMocks: true,
+  restoreMocks: true,
+};
