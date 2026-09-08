b36c8059cebc7f2a740cd28a5a4e9a2ce854f27f
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 09:58:50 +0400

    feat(machine-connect): wire connector sync dependencies

--- machine-connect/core/src/connectors/connector.module.ts
+++ machine-connect/core/src/connectors/connector.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { TelemetryModule } from '../telemetry/telemetry.module';
+import { IntelligenceModule } from '../intelligence/intelligence.module';
 import { ConnectorController } from './connector.controller';
 import { ConnectorService } from './connector.service';
 import { ConnectorRegistry } from './connector.registry';
+import { ConnectorSyncService } from './connector-sync.service';
 
 @Module({
-  imports: [PersistenceModule],
+  imports: [PersistenceModule, TelemetryModule, IntelligenceModule],
   controllers: [ConnectorController],
-  providers: [ConnectorService, ConnectorRegistry],
-  exports: [ConnectorService, ConnectorRegistry],
+  providers: [ConnectorService, ConnectorRegistry, ConnectorSyncService],
+  exports: [ConnectorService, ConnectorRegistry, ConnectorSyncService],
 })
 export class ConnectorModule {}
