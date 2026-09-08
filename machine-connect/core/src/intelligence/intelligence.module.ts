9df8e4c3c72caa9ec74c76905082417163027f12
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 09:44:04 +0400

    feat: connect intelligence queue to document processor

--- machine-connect/core/src/intelligence/intelligence.module.ts
+++ machine-connect/core/src/intelligence/intelligence.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
+import { DocumentModule } from '../graph/document.module';
 import { IntelligenceJobController } from './intelligence-job.controller';
 import { IntelligenceJobService } from './intelligence-job.service';
 
-@Module({imports:[PersistenceModule],controllers:[IntelligenceJobController],providers:[IntelligenceJobService],exports:[IntelligenceJobService]})
+@Module({imports:[PersistenceModule,DocumentModule],controllers:[IntelligenceJobController],providers:[IntelligenceJobService],exports:[IntelligenceJobService]})
 export class IntelligenceModule {}
