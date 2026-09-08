6c0ef79d586a6151f8278297c51100160a53338f
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 21:02:55 +0400

    feat(machine-connect): wire fleet intelligence into core

--- machine-connect/core/src/app.module.ts
+++ machine-connect/core/src/app.module.ts
@@ -29,9 +29,10 @@ import { IntelligenceModule } from './intelligence/intelligence.module';
 import { TwinModule } from './twin/twin.module';
 import { OperationsModule } from './operations/operations.module';
 import { FleetModule } from './fleet/fleet.module';
+import { FleetIntelligenceModule } from './fleet-intelligence/fleet-intelligence.module';
 
 @Module({
-  imports: [AdaptersModule, MachineCredentialsModule, MachineModule, CommandModule, TelemetryModule, AuditModule, EmergencyStopModule, OffensiveSecurityModule, RemediationModule, SecurityModule, AdvancedModule, WorkflowModule, ServiceRequestModule, RealtimeModule, SyncModule, GraphModule, AdvancedAnalyticsModule, DocumentModule, DocumentEntityModule, ComplianceModule, SecurityEventModule, ApiKeyModule, ConnectorModule, IntelligenceModule, TwinModule, OperationsModule, FleetModule],
+  imports: [AdaptersModule, MachineCredentialsModule, MachineModule, CommandModule, TelemetryModule, AuditModule, EmergencyStopModule, OffensiveSecurityModule, RemediationModule, SecurityModule, AdvancedModule, WorkflowModule, ServiceRequestModule, RealtimeModule, SyncModule, GraphModule, AdvancedAnalyticsModule, DocumentModule, DocumentEntityModule, ComplianceModule, SecurityEventModule, ApiKeyModule, ConnectorModule, IntelligenceModule, TwinModule, OperationsModule, FleetModule, FleetIntelligenceModule],
   controllers: [HealthController],
 })
 export class AppModule implements NestModule {
