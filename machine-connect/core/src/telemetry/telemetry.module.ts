a90c14c27790daf49c61d7f99d50264694eee375
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 14:17:56 +0400

    feat(machine-connect): wire telemetry twin bridge

--- machine-connect/core/src/telemetry/telemetry.module.ts
+++ machine-connect/core/src/telemetry/telemetry.module.ts
@@ -3,6 +3,12 @@ import { TelemetryController } from './telemetry.controller';
 import { TelemetryService } from './telemetry.service';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { MachineModule } from '../machine/machine.module';
+import { TwinModule } from '../twin/twin.module';
 
-@Module({ imports: [PersistenceModule, MachineModule], controllers: [TelemetryController], providers: [TelemetryService], exports: [TelemetryService] })
+@Module({
+  imports: [PersistenceModule, MachineModule, TwinModule],
+  controllers: [TelemetryController],
+  providers: [TelemetryService],
+  exports: [TelemetryService],
+})
 export class TelemetryModule {}
