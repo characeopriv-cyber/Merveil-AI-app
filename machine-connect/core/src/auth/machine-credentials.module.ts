bc2a13706d25c9c65fefbad3075cd267dd7efeb7
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 21:56:01 +0400

    feat(machine-connect): register EMQX auth controller

--- machine-connect/core/src/auth/machine-credentials.module.ts
+++ machine-connect/core/src/auth/machine-credentials.module.ts
@@ -3,11 +3,12 @@ import { PersistenceModule } from '../persistence/persistence.module';
 import { MachineModule } from '../machine/machine.module';
 import { SecurityEventModule } from '../security/security-event.module';
 import { MachineCredentialsController } from './machine-credentials.controller';
+import { MqttAuthController } from './mqtt-auth.controller';
 import { MachineCredentialsService } from './machine-credentials.service';
 
 @Module({
   imports: [PersistenceModule, MachineModule, SecurityEventModule],
-  controllers: [MachineCredentialsController],
+  controllers: [MachineCredentialsController, MqttAuthController],
   providers: [MachineCredentialsService],
   exports: [MachineCredentialsService],
 })
