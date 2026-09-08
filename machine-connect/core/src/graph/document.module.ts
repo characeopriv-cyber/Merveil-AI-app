cf40985604b9a4c72a41ed277cc9e0b94c532980
IVONIX CEO <characeopriv@gmail.com>
2026-09-08 08:11:38 +0400

    feat: register document processor service

--- machine-connect/core/src/graph/document.module.ts
+++ machine-connect/core/src/graph/document.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
+import { DocumentProcessorService } from './document-processor.service';
 
-@Module({ imports: [PersistenceModule], controllers: [DocumentController], providers: [DocumentService] })
+@Module({ imports: [PersistenceModule], controllers: [DocumentController], providers: [DocumentService, DocumentProcessorService], exports: [DocumentProcessorService] })
 export class DocumentModule {}
