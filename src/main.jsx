ae78e53aeb6d76fd4490d71bf8b5809dc606fc76
Merveil AI <characeopriv@gmail.com>
2026-09-09 19:07:31 +0400

    Start Merveil notification system with realtime

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { startMerveilRealtime } from "./merveilRealtime.js";
+import { startMerveilNotifications } from "./merveilNotifications.js";
 
 startMerveilRealtime();
+startMerveilNotifications();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
