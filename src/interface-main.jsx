aafc0ddbc512987e1d4241ea1edecbc02bdc3d6a
Merveil AI <characeopriv@gmail.com>
2026-09-13 01:38:35 +0400

    fix(interface): mount dedicated Interface platform entry

---
+++ src/interface-main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MerveilInterfacePlatform from "./MerveilInterfacePlatform.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MerveilInterfacePlatform />
+  </React.StrictMode>
+);
