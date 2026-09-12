a491aba112d7e5c55d8450414d01405195b2d1ae
Merveil AI <characeopriv@gmail.com>
2026-09-12 19:41:01 +0400

    feat(boost): start shared Boost overlay on Interface

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import "./index.css";
 import { startMerveilRealtime } from "./merveilRealtime.js";
 import { startMerveilNotifications } from "./merveilNotifications.js";
 import { startMerveilProductionFeeds } from "./merveilProductionFeeds.js";
+import { startMerveilBoost } from "./merveilBoostGlobal.js";
 
 startMerveilRealtime();
 startMerveilNotifications();
 startMerveilProductionFeeds();
+startMerveilBoost();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
